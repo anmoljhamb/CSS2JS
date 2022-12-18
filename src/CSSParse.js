341933dd730561763573c4f8f5e27da9310e3749
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-18 16:59:50 +0530

    regex to find all the selectors and the content of their properties

--- src/CSSParse.js
+++ src/CSSParse.js
@@ -1,36 +1,38 @@
 class Property {
     // Property, like color: white;
-    constructor(name, value){
+    static pattern = /(.+):(.+);/
+    constructor(name, value) {
         this.name = name;
         this.value = value;
     }
 
-    toString(){
+    toString() {
         return `${this.name}: ${this.value};`
     }
 }
 
 
-class Selector{
+class Selector {
     // the selectory query for a css BLOCK.
     // for example, .container 
-    constructor(query){
+    static pattern = /(.+)\{/
+    constructor(query) {
         this.query = query;
-        this.properties = [] 
+        this.properties = []
     }
 
-    
-    addProperty(property){
+
+    addProperty(property) {
         this.properties.push(property)
     }
 
 
-    getProperty(n){
+    getProperty(n) {
         return this.properties[n]
     }
 
 
-    getProperties(){
+    getProperties() {
         return this.properties
     }
 
@@ -45,11 +47,39 @@ class Selector{
 }
 
 
-class CSSParse{
-    constructor(cssString)
-    {
+
+const findAll = (re, string) => {
+    re = new RegExp(re, "g")
+    const matches = []
+    let match = re.exec(string)
+    while (match != null) {
+        matches.push(match)
+        match = re.exec(string)
+        break
+    }
+    return matches
+}
+
+
+class CSSParse {
+    constructor(cssString) {
         // Takes in cssString as constructor.
         this.cssString = cssString;
+        // find all selectors
+        const selectors = findAll(Selector.pattern, this.cssString)
+        selectors.forEach((selector) => {
+
+            /*
+                startIndex: index of { from selector {property: value} 
+                endIndex: index of } from selector { property: value}
+                patternString = cssString[startIndex:startIndex+endIndex] 
+            */
+
+            const startIndex = selector.index + selector[0].length
+            console.log(selector);
+            const endIndex = /\}/.exec(this.cssString.slice(startIndex)).index
+            const patternString = this.cssString.slice(startIndex, startIndex + endIndex)
+        })
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+const CSSParse = require("./CSSParse")
+const fs = require("fs")
+const path = require("path")
+
+let cssString = fs.readFileSync(path.join(__dirname, "sample.css"), "utf-8")
+cssParse = new CSSParse(cssString)
