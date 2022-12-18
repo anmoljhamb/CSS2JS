e5962c606b7ce7769ba49e638d0a714f86451931
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-18 23:31:17 +0530

    css for the elements

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,9 @@
-console.log("Home Page");
+const css = document.querySelector("textarea.css")
+const js = document.querySelector("textarea.js")
+
+
+document.querySelector("button")
+    .addEventListener("click", () => {
+        console.log(css.value);
+        js.value = css.value
+    })
