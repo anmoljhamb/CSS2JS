b4562005b0ffb1fb84aa43991642d1d12c54baed
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-19 00:15:13 +0530

    Add a clipboard to copy code

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,8 @@
 const css = document.querySelector("textarea.css")
 const js = document.querySelector("textarea.js")
+const button = document.querySelector("button")
 
-
-document.querySelector("button")
-    .addEventListener("click", async () => {
+button.addEventListener("click", async () => {
         const resp = await fetch("/convert", {
             method: "POST",
             headers: {'Content-Type': 'application/json'},
@@ -11,4 +10,43 @@ document.querySelector("button")
         })
         const data = await resp.json()
         js.value = data.converted
+    })
+
+
+const resetButton = () => {
+    button.innerText = "Convert"
+    button.classList.toggle("active")
+}
+
+
+const copied = () => {
+    button.innerText = "Copied!"
+    button.classList.toggle("active")
+}
+
+document.querySelector(".left i")
+    .addEventListener("click", async () => {
+        copied()
+        await navigator.clipboard.writeText(css.value)
+            .then(() => {
+                setTimeout(resetButton, 800)
+                console.log("Copied to clipboard successfully");
+            })
+            .catch((err) => {
+                console.log("There was an error while copying.");
+            })
+    })
+
+
+document.querySelector(".right i")
+    .addEventListener("click", async () => {
+        copied()
+        await navigator.clipboard.writeText(js.value)
+            .then(() => {
+                setTimeout(resetButton, 800)
+                console.log("Copied to clipboard successfully");
+            })
+            .catch((err) => {
+                console.log("There was an error while copying.");
+            })
     })
